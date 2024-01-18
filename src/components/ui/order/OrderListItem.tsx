88b0cd98c77e2bbbde0b574c8ba66b374dbe63d5
zeriong <jaeryong95@gmail.com>
2024-01-19 02:35:30 +0900

    주문 리스트 recoil 적용

--- src/components/ui/order/OrderListItem.tsx
+++ src/components/ui/order/OrderListItem.tsx
@@ -1,10 +1,14 @@
 'use client';
 
 import {TItem} from "@/store/order/order.atoms";
+import {orderQuantityState} from '@/store/order/order.selectors';
+import {useRecoilState} from 'recoil';
+import {numberSeparator} from '@/utils/common.utils';
 
 export const OrderListItem = ({ item } : { item: TItem }) => {
+    const [quantity, addQuantity] = useRecoilState(orderQuantityState(item));
     return (
-        <li className='flex gap-5 py-[8px] px-[12px] rounded-2xl border border-gray-300'>
+        <li className={`flex gap-5 py-[8px] px-[12px] rounded-[15px] border border-black/30 ${quantity > 0 ? 'bg-[#F75A2F] bg-opacity-10' : ''}`}>
             <div className='w-[62px] h-[62px] bg-[#D9D9D9]'></div>
             <div className='grow flex flex-col justify-between'>
                 <div className='flex gap-[8px] items-center h-[22px] w-full'>
@@ -14,18 +18,18 @@ export const OrderListItem = ({ item } : { item: TItem }) => {
                     }
                 </div>
                 <div className='flex justify-between w-full'>
-                    <div className='flex gap-[3px]'>
-                        <button>
+                    <div className="flex gap-[4px]">
+                        <button onClick={() => addQuantity(-1)}>
                             -
                         </button>
-                        <p>
-                            {0}
+                        <p className='ml-[1px]'>
+                        {quantity}
                         </p>
-                        <button>
+                        <button onClick={() => addQuantity(+1)}>
                             +
                         </button>
                     </div>
-                    <p>{0}원</p>
+                    <p>{numberSeparator(item.price)}원</p>
                 </div>
             </div>
         </li>
