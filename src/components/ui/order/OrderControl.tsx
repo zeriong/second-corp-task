abe1e50be352bb4c1ad54483c291f80f6c74f364
zeriong <jaeryong95@gmail.com>
2024-01-19 02:39:01 +0900

    주문하기 UI에 recoil 적용

--- src/components/ui/order/OrderControl.tsx
+++ src/components/ui/order/OrderControl.tsx
@@ -3,6 +3,7 @@
 import {Submit} from '@/components/Submit';
 import {totalOrderState} from '@/store/order/order.selectors';
 import {useRecoilValue} from 'recoil';
+import {numberSeparator} from '@/utils/common.utils';
 
 export const OrderControl = () => {
     const totalState = useRecoilValue(totalOrderState);
@@ -10,10 +11,10 @@ export const OrderControl = () => {
         <form className='flex flex-col justify-between fixed left-0 bottom-0 w-full h-[170px] bg-white p-[25px] rounded-t-2xl shadow-[0px_-4px_10px_0px_rgba(0,0,0,0.25)]'>
             <div className='flex flex-col items-end'>
                 <div>
-                    총 수량: {0}개
+                    총 수량: {totalState.totalQuantity}개
                 </div>
                 <div>
-                    총 가격: {0}원
+                    총 가격: {numberSeparator(totalState.totalPrice)}원
                 </div>
             </div>
             <Submit
